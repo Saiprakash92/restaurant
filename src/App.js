27484ea0e4e275fa28305c1cecf8dee8272d8fbf
Prakash singh <ps299106@gmail.com>
2023-01-29 22:00:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import MenuCard from "./component/Basics/MenuCard";
+import Resturant from "./component/Basics/Resturant";
+import UseEffect from "./component/Hooks/useEffect";
+// import Resturant from "./component/Basics/Resturant";cd 
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+import UseReducer from "./component/Hooks/useReducer";
+import Navbar from "./component/Basics/Navbar";
+const App = () => {
+
+  return (
+    <>
+      {/* <UseReducer />; */}
+      {/* <UseEffect/> */}
+      
+      <Resturant/>
+    </>
+  )
+};
+
+export default App;
